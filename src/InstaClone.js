2725e50fbf7ce944fb73fbb8d4b3b5355acdf3cd
Magnus Arbuthnott <magnusarbuthnott@gmail.com>
2019-09-16 15:18:13 +0100

    refactored navigation

--- src/InstaClone.js
+++ src/InstaClone.js
@@ -1,5 +1,15 @@
 import React, { Component } from "react"
-import AppNavigator from "./components/navigation/MainTabNavigator"
+// import AppNavigator from "./components/navigation/MainTabNavigator"
+import { createAppContainer } from "react-navigation"
+import { createBottomTabNavigator } from "react-navigation-tabs"
+import { MainFeed, Login } from "./components/screens"
+
+const TabNavigator = createBottomTabNavigator({
+  login: Login,
+  feed: MainFeed
+})
+
+const AppNavigator = createAppContainer(TabNavigator)
 
 class InstaClone extends Component {
 
